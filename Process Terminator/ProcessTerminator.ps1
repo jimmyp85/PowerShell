88a069f62f554b459fcd26ea8ccca0fe1f5d4665
jimmyp85 <40490567+jimmyp85@users.noreply.github.com>
2021-01-07 11:57:17 +0000

    Update ProcessTerminator.ps1

--- Process Terminator/ProcessTerminator.ps1
+++ Process Terminator/ProcessTerminator.ps1
@@ -1,1 +1,29 @@
+$ErrorActionPreference = 'SilentlyContinue'
 
+Add-Type -AssemblyName "PresentationFramework"
+
+$msgBoxInput = [System.Windows.MessageBox]::Show('ENTER MESSAGE HERE','ENTER BOX TITLE HERE','YesNoCancel','Error')
+
+  switch  ($msgBoxInput) {
+
+  'Yes' {
+
+ $Processes = Get-Content -Path "\\SHARE\FILE.TXT"
+
+
+Get-Process -Name $Processes | Stop-Process -Force -ErrorAction $ErrorActionPreference
+
+  }
+
+  'No' {
+
+  Exit
+
+  }
+  }
+
+[System.Windows.MessageBox]::Show('Termination has been completed. Please exit.','ENTER BOX TITLE') | Out-Null
+
+
+
+© 2021 GitHub, Inc.
