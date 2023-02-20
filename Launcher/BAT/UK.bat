4d2adcd79512c0272db461593ec8ca9675bd079b
jimmyp85 <40490567+jimmyp85@users.noreply.github.com>
2023-02-20 14:32:22 +0000

    Update and rename UKBS.bat to UK.bat

---
+++ Launcher/BAT/UK.bat
@@ -0,0 +1,3 @@
+REG ADD HKEY_CURRENT_USER\SOFTWARE\Brokasure\Docview\Forms /v Path /d "\\fs\Brokasure\Forms\Live" /f
+
+C:\BSX\APPLICS\logon.exe /H=10.10.10.10 /S="ponm" /L=/x/sadmlog
--- Launcher/BAT/UKBS.bat
+++
@@ -1,3 +0,0 @@
-REG ADD HKEY_CURRENT_USER\SOFTWARE\Brokasure\Docview\Forms /v Path /d "\\fs01\Brokasure\Forms\Live" /f
-
-C:\BSX\APPLICS\logon.exe /H=192.168.5.245 /S="pirb" /L=/x/sadmlog
